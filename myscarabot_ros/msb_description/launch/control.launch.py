cc9a887262d8c356b78aca475776029671cf142b
Sampreet <sampreets3@gmail.com>
2022-09-21 18:53:42 +0200

    Major changes, updated Dockerfile, added control.launch.py

--- myscarabot_ros/msb_description/launch/control.launch.py
+++ myscarabot_ros/msb_description/launch/control.launch.py
@@ -1,21 +1,20 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
+import xacro
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
-from launch.conditions import IfCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration, PythonExpression, Command
 from launch_ros.actions import Node
-
 
 def generate_launch_description():
     # Get the launch directory
     bringup_dir = get_package_share_directory('msb_description')
     launch_dir = os.path.join(bringup_dir, 'launch')
     rviz_config = os.path.join(bringup_dir, 'rviz', 'config.rviz')
-    urdf_file 	= os.path.join(bringup_dir, 'urdf', 'scarabot.urdf') 
+    urdf_file 	= os.path.join(bringup_dir, 'urdf', 'scarabot.urdf')
+    xacro_file = os.path.join(bringup_dir, 'xacro', 'scarabot.xacro') 
+    controllers_file = os.path.join(get_package_share_directory('msb_control'), 'controllers', 'myscarabot_controllers.yaml')
+    robot_description = {'robot_description' : Command(['xacro',' ', xacro_file ])}
 
     start_robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
@@ -23,7 +22,9 @@ def generate_launch_description():
         name='robot_state_publisher',
         output='screen',
         #parameters=[{'use_sim_time': use_sim_time}],
-        arguments=[urdf_file])
+        # arguments=[urdf_file]
+        parameters=[robot_description],
+        )
     
     start_joint_state_publisher_cmd = Node(
         package='joint_state_publisher_gui',
@@ -38,7 +39,14 @@ def generate_launch_description():
         name='rviz2',
         arguments=['-d', rviz_config],
         output='screen')
-        
+
+    controller_manager = Node(
+        package="controller_manager", 
+        executable="ros2_control_node", 
+        parameters=[robot_description, controllers_file], 
+        output='screen'
+    )
+
     # Create the launch description and populate
     ld = LaunchDescription()
 
@@ -46,5 +54,6 @@ def generate_launch_description():
     ld.add_action(start_joint_state_publisher_cmd)
     ld.add_action(start_robot_state_publisher_cmd)
     ld.add_action(rviz_cmd)
+    ld.add_action(controller_manager)
 
-    return ld   
+    return ld
