1fe474c36326e14c1c740fdac2f8184c13794d11
Sampreet SARKAR <sampreets3@gmail.com>
2024-08-30 15:54:36 +0200

    Reduced robot size, added collision geometry

--- myscarabot_ros/msb_description/launch/display.launch.py
+++ myscarabot_ros/msb_description/launch/display.launch.py
@@ -6,7 +6,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import Command
 from launch_ros.actions import Node
 
 
@@ -17,20 +17,22 @@ def generate_launch_description():
     rviz_config = os.path.join(bringup_dir, 'rviz', 'config.rviz')
     urdf_file 	= os.path.join(bringup_dir, 'urdf', 'scarabot.urdf') 
 
+    robot_description_param = Command(['xacro ', os.path.join(bringup_dir, 'xacro', 'scarabot.xacro')])
+
     start_robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        #parameters=[{'use_sim_time': use_sim_time}],
-        arguments=[urdf_file])
+        parameters=[{'robot_description': robot_description_param, 'use_sim_time': True}]
+        )
     
     start_joint_state_publisher_cmd = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
         output='screen',
-        arguments=[urdf_file])
+        parameters=[{'robot_description': robot_description_param}])
     
     rviz_cmd = Node(
         package='rviz2',
