8975da319c97954ed2bdaaac9d8a1cf5e0dbb7e7
Sampreet SARKAR <sampreets3@gmail.com>
2025-01-20 17:28:26 +0100

    added simple subscriber to joint_state

--- myscarabot_ros/msb_control/include/msb_control/msbFKinDemo.hpp
+++ myscarabot_ros/msb_control/include/msb_control/msbFKinDemo.hpp
@@ -6,15 +6,19 @@
 #include <iostream>
 
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 
 class MyScaraBotForwardKinematicsNode : public rclcpp::Node {
 
 public:
     MyScaraBotForwardKinematicsNode();
-    void timer_callback();
 
 private:
     rclcpp::TimerBase::SharedPtr timer_;
     int                          count_;
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr m_jointStateSub;
+
+    void timer_callback();
+    void SubCb(const sensor_msgs::msg::JointState &msg);
 };
 
--- myscarabot_ros/msb_control/src/msbFKinDemo.cpp
+++ myscarabot_ros/msb_control/src/msbFKinDemo.cpp
@@ -1,12 +1,24 @@
 #include "msb_control/msbFKinDemo.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 
 using namespace std::chrono_literals;
 
 MyScaraBotForwardKinematicsNode::MyScaraBotForwardKinematicsNode() : Node("msb_fk_control") {
     RCLCPP_INFO(this->get_logger(), "Hello, from forward kinematics node.");
     timer_ = this->create_wall_timer(1s, std::bind(&MyScaraBotForwardKinematicsNode::timer_callback, this));
+
+    m_jointStateSub = this->create_subscription<sensor_msgs::msg::JointState>("/joint_states", 10, std::bind(&MyScaraBotForwardKinematicsNode::SubCb, this, std::placeholders::_1));
 }
 
 void MyScaraBotForwardKinematicsNode::timer_callback() {
     RCLCPP_INFO(this->get_logger(), "Hello, this is from the timer.");
+}
+
+void MyScaraBotForwardKinematicsNode::SubCb(const sensor_msgs::msg::JointState &msg)
+{
+    for (int i = 0; i < msg.name.size(); i++)
+    {
+        RCLCPP_INFO(this->get_logger(), "Joint: %s   Value : %.3f", msg.name[i].c_str(), msg.position[i]);
+    }
+    RCLCPP_INFO(this->get_logger(), "---------------------");
 }
